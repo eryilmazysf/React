2b6025564558bdd7ad2d1237258ba7e1c4c4a05e
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2022-01-05 18:19:32 -0500

    podfile updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from "react-native";
 import App from "./src/ElectronicShopping/Project1";
+//import App from "./src/lesson/Main";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
